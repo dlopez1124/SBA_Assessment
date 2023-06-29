a997f97d9f155fc97daf97334b36385d34ba55b2
David Lopez <dl1124@georgetown.edu>
2023-06-29 15:08:27 -0400

    Cleaned code for .py

--- streamlit_wosb.py
+++ streamlit_wosb.py
@@ -4,7 +4,7 @@
 # In[1]:
 
 
-
+######### Import packages
 
 import pandas as pd
 import streamlit as st
@@ -16,14 +16,14 @@ warnings.filterwarnings('ignore')
 
 
 
+####### Load state data from data manipulation
+
+
 wosb_by_state = pd.read_csv('Data/wosb_by_state.csv')
 
 wosb_gdf = wosb_by_state
 
-#wosb_gdf = state_gdf.merge(wosb_by_state, how='left', on='Entity State')
-#wosb_gdf = wosb_gdf.dropna()
 
-#wosb_gdf = wosb_gdf.sort_values(by='Entity State').reset_index(drop=True)
 
 wosb_gdf['Total WOSB Action Obligation'] = wosb_gdf['Total WOSB Action Obligation'].apply(lambda x: '${:,.2f}'.format(x))
 wosb_gdf['EDWOSB Action Obligation'] = wosb_gdf['EDWOSB Action Obligation'].apply(lambda x: '${:,.2f}'.format(x))
@@ -32,12 +32,8 @@ wosb_gdf['Eligible NAICS Action Obligation'] = wosb_gdf['Eligible NAICS Action O
 
 
 
-#s = gpd.GeoSeries(wosb_gdf['geometry'])
 
-#x_map = s.centroid.x.mean()
-#y_map = s.centroid.y.mean()
-
-pd.options.display.float_format = '{:.2f}'.format
+#pd.options.display.float_format = '{:.2f}'.format
 
 st.title('SBA WOSB Analysis')
 st.subheader('By: David Lopez Jr.')
@@ -51,6 +47,7 @@ st.markdown('##### Please use the following hyperlink to view the [WOSB Analysis
 st.markdown('')
 
 
+
 df_sorted = wosb_by_state.sort_values('Prop Ineligible', ascending=False)
 df_sorted['Prop Ineligible'] = pd.to_numeric(df_sorted['Prop Ineligible'])
 df_sorted['Proportion of WOSB Funds Obligated to Ineligible NAICS Codes'] = df_sorted['Prop Ineligible']
@@ -60,6 +57,8 @@ df_sorted['Proportion of WOSB Funds Obligated to Ineligible NAICS Codes'] = df_s
 # Get the top 15 states
 top_15_states = df_sorted[['Entity State', 'Prop Ineligible']].head(15).reset_index(drop=True)
 
+# Create altair barchart
+
 chart = alt.Chart(top_15_states).mark_bar().encode(y=alt.Y('Entity State', sort='-x'),
     x='Prop Ineligible',
     tooltip=['Entity State', 'Prop Ineligible']).properties(width=600,
@@ -68,36 +67,9 @@ chart = alt.Chart(top_15_states).mark_bar().encode(y=alt.Y('Entity State', sort=
 st.altair_chart(chart, use_container_width=True)
 
 
-def display_state_info(state):
-    if state == 'Alabama':
-        st.header('Information about Alabama')
-        # Display specific information or perform actions for Alabama
-    elif state == 'Alaska':
-        st.write('Information about Alaska')
-        # Display specific information or perform actions for Alaska
-    elif state == 'Arizona':
-        st.write('Information about Arizona')
-        # Display specific information or perform actions for Arizona
-    elif state == 'Arkansas':
-        st.write('Information about Arkansas')
-        # Display specific information or perform actions for Arkansas
-    elif state == 'California':
-        st.write('Information about California')
-        # Display specific information or perform actions for California
-    # Add elif statements for the remaining states, Washington D.C., Puerto Rico, and Guam
-    elif state == 'Washington D.C.':
-        st.write('Information about Washington D.C.')
-        # Display specific information or perform actions for Washington D.C.
-    elif state == 'Puerto Rico':
-        st.write('Information about Puerto Rico')
-        # Display specific information or perform actions for Puerto Rico
-    elif state == 'Guam':
-        st.write('Information about Guam')
-        # Display specific information or perform actions for Guam
-
-# List of states, including Washington D.C., Puerto Rico, and Guam
 
 def display_state_info(state):
+    '''Takes the input from st selectbox and displays state info'''
     if state != 'Click Here to Select a State or Territory':
         state_info = wosb_gdf[wosb_gdf['Entity State'] == state]
         st.subheader(f"WOSB Information for {state}")
@@ -105,6 +77,7 @@ def display_state_info(state):
         st.write(f"EDWOSB Funds Obligated to Eligible NAICS Codes: {state_info['EDWOSB Action Obligation'].values[0]}")
         st.write(f"WOSB Funds Obligated to Eligible NAICS Codes: {state_info['Eligible NAICS Action Obligation'].values[0]}")
         st.write(f"WOSB Funds Obligated to Ineligible NAICS Codes: {state_info['Ineligible NAICS Action Obligation'].values[0]}")
+
 
 # List of states, including Washington D.C., Puerto Rico, and Guam
 states = ['Click Here to Select a State or Territory','Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia',
